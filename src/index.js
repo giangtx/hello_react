acbcfbdca2cec202e03bb120a6661c90e1c34890
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-02-28 22:49:57 +0700

    modal loading

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import './styles/app.scss';
 import { I18nextProvider } from 'react-i18next';
 import i18n from "./lang";
 import { Provider } from 'react-redux';
-import store from "./store";
+import store from './store';
 
-ReactDOM.render(
-  // I18nextProvider setup midlware cho toàn bộ component nhận giá trị từ folder lang
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <React.Fragment>
-          <App />
-        </React.Fragment>
-      </I18nextProvider>,
+        <I18nextProvider i18n={i18n}>
+            <React.Fragment>
+                <App />
+            </React.Fragment>
+        </I18nextProvider>
     </Provider>
-  //document.getElementById("root")
 );
 
 
